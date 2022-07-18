1fa241dbc2851bd53f0b59c9f018bbc08230f7f4
Jinshi He <jih064@ucsd.edu>
2022-07-17 20:03:16 -0700

    Updates

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const APIAI_TOKEN = 
+
 const express = repquire('express');
 const app = express();
 
@@ -5,6 +7,9 @@ app.use(express.static(_dirname + '/views')); // html
 app.use(express.static(_dirname + '/public')); // js, css, images
 
 const server = app.listen(5000);
+
+const apiai = require('apiai')(APIAI_TOKEN);
+
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
---
+++ public/js/script.js
@@ -0,0 +1,22 @@
+const socket = io();
+
+const SpeechRecognition = window.SpeechRecognition || 
+    window.webkitSpeechRecognition;
+const recogntion = new SpeechRecognition();
+
+recogntion.lang = 'en-US';
+recogntion.interimResults = false;
+
+document.querySelector('button').addEventListener('click', () => {
+    recogntion.start();
+});
+
+recogntion.addEventListener('result', (e) => {
+    let last = e.results.length - 1;
+    let text = e.results[last][0].transcript;
+
+    console.log('Confidence: ' + e.results[0][0].confidence);
+
+    socket.emit('chat message', text);
+});
+
